028199b4070d5494e902a9a16451af847fa90d9e
Saba-Var <vartasashvili94@gmail.com>
2022-05-19 19:07:43 +0400

    chore: change placeholder of repeat password input

--- src/components/RepeatPassword.tsx
+++ src/components/RepeatPassword.tsx
@@ -1,16 +1,19 @@
 import { useTranslation } from 'react-i18next'
 import { ErrorMessage } from 'components'
 
-const RepeatPassword: React.FC<{
+type propsType = {
   required: string
   register: any
   label: string
   valid: string
   errors: any
   data: any
-}> = (props) => {
-  const { t } = useTranslation()
+}
+
+const RepeatPassword: React.FC<propsType> = (props) => {
   const match = props.data['Repeat Password'] !== props.data.Password
+  const { t } = useTranslation()
+
   return (
     <div className='flex flex-col gap-2 '>
       <label className='text-black'>{t(props.label)}</label>
@@ -22,14 +25,12 @@ const RepeatPassword: React.FC<{
         className={`border-border-gray border-[1px] h-14 px-6 rounded-lg focus:ring-2 focus:border-link-blue outline-none ${
           (props.errors || match) && 'border-red border-2'
         } bg-transparent`}
-        placeholder={t('Email Placeholder')}
+        placeholder={t('Repeat Password')}
       />
       {props.errors && !match && (
         <ErrorMessage text={t(props.errors.message)} error={props.errors} />
       )}
-      {match && (
-        <ErrorMessage text={t('Password match')} error={props.errors} />
-      )}
+      {match && <ErrorMessage text={t('Password match')} error={match} />}
     </div>
   )
 }
