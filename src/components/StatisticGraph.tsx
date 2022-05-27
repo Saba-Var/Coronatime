e1a9ace08ef21b478e75bbd3c23a531b2b4b1bad
Saba-Var <vartasashvili94@gmail.com>
2022-05-27 14:12:20 +0400

    chore: change styling of dashboard graphs

--- src/components/StatisticGraph.tsx
+++ src/components/StatisticGraph.tsx
@@ -6,11 +6,13 @@ const StatisticGraph: React.FC<StatisticsProps> = (props) => {
 
   return (
     <div
-      className={`py-6 h-[193px] xl:w-[100%] xl:h-[255px] rounded-2xl items-center justify-between flex flex-col ${props.containerStyle}`}
+      className={`py-6 h-[193px] lg:w-[100%] lg:h-[255px] items-center justify-between flex flex-col ${props.containerStyle} rounded-2xl`}
     >
       <img src={props.image} alt='ss' className='w-[90px] h-[64px] ' />
-      <p>{t(props.target)}</p>
-      <p className={`text-2xl ${props.textStye} font-black`}>
+      <p className='text-xl font-medium'>{t(props.target)}</p>
+      <p
+        className={`text-2xl lg:text-3xl xl:text-4xl ${props.textStye} font-black`}
+      >
         {props.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
       </p>
     </div>
--- src/pages/Worldwide/Worldwide.tsx
+++ src/pages/Worldwide/Worldwide.tsx
@@ -17,8 +17,8 @@ const Worldwide = () => {
   return (
     <>
       <Categories title='Worldwide' />
-      <div className='flex justify-center xl:block'>
-        <div className='px-4 md:mx-12 xl:px-28 grid grid-cols-2 col-span-1 xl:flex justify-between xl:gap-6 gap-y-4'>
+      <div className='flex justify-center lg:block'>
+        <div className='px-4 md:mx-12 md:mt-10 xl:px-28 grid grid-cols-2 col-span-1 lg:flex justify-between lg:gap-6 gap-y-4'>
           <StatisticGraph
             containerStyle='bg-blue w-[343px] md:m-0  col-span-2'
             textStye='text-link-blue'
@@ -27,14 +27,14 @@ const Worldwide = () => {
             image={rising}
           />
           <StatisticGraph
-            containerStyle='bg-greenBG w-[164px] xl:h-[255px]'
+            containerStyle='bg-greenBG w-[164px] lg:h-[255px]'
             textStye='text-green'
             total={totalRecovered}
             target='recovered'
             image={recovered}
           />
           <StatisticGraph
-            containerStyle='bg-yellowBG w-[164px] xl:h-[255px]'
+            containerStyle='bg-yellowBG w-[164px] lg:h-[255px]'
             textStye='text-yellow'
             total={totalDeath}
             target='deaths'
