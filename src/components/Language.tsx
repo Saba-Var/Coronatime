d30c8aa19603d134af9f1bfd26c7fde88c00d388
Saba-Var <vartasashvili94@gmail.com>
2022-05-28 17:30:57 +0400

    feat: save selected language in the local storage

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react'
 import i18n from 'i18next'
 
 const Language: React.FC<LanguageProps> = (props) => {
-  const [language, setLanguage] = useState<string>('en')
+  const [language, setLanguage] = useState<string>(
+    localStorage.getItem('Language') || 'en'
+  )
   const { t } = useTranslation()
   const changeLanguage = props.setLanguage
 
@@ -16,13 +18,20 @@ const Language: React.FC<LanguageProps> = (props) => {
   return (
     <div>
       <select
+        value={language}
+        id='Languages'
         data-TestId='Language'
-        onChange={(e) => setLanguage(e.target.value)}
+        onChange={(e) => {
+          setLanguage(e.target.value)
+          localStorage.setItem('Language', e.target.value)
+        }}
         name='languages'
         className={`border-0 form-select form-select-lg
-        appearance-none block w-full px-4 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border-solid border-border-gray rounded transition ease-in-out focus:text-gray-700focus:border-blue-600 focus:outline-none hover:scale-110 cursor-pointer`}
+        appearance-none block w-full px-4 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border-solid border-border-gray rounded transition ease-in-out focus:text-gray-700focus:border-blue-600 focus:outline-none hover:scale-110 cursor-pointer `}
       >
-        <option value='en'>{t('English')}</option>
+        <option value='en' onClick={() => i18n.changeLanguage('en')}>
+          {t('English')}
+        </option>
         <option value='ge' onClick={() => i18n.changeLanguage('ge')}>
           {t('Georgian')}
         </option>
